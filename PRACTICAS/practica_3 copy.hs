34ffeb061edf2b36a43fbc30fa237c38a58535f6
Ignacio <ignacioiannello7@gmail.com>
2024-09-24 21:31:50 -0300

    VAMOOOO0OO

--- PRACTICAS/practica_3 copy.hs
+++ PRACTICAS/practica_3 copy.hs
@@ -85,18 +85,128 @@ Si M → N , entonces:
     V M → V N (ν)
     if M then O else P → if N then O else P (ifc)
 
+axv             T-true      T-var 
+---------- ------------- ----------
+GAMMA|-X:b GAMMA|-TRUE:B GAMMA|-y:b          IF
+-----------------------------------------------
+GAMMA=x:b,y:b|-if x then true else y):b    t-abs
+----------------------------------------------
+|-λy:b if x then true else y):b->b       t-abs
+---------------------------------------------  ----------- axs
+|- (λx:b, λy:b if x then true else y):b->b->B  |-FALSE:b
+----------------------------------------------- ->e
+|-(λx:b, λy:b if x then true else y) false :b>b
+
+B) 
+----------------- axt
+x:bool|-true:bool
+
+C) no tiene ningun juiicio de tipado valido 
+                                AXV
+                            ----------   ----------AXB
+                            gamma|-x:σ -> y:σ           AXV
+                            --------------------  -----------------------------  
+            AXV              GAMMA|-XY: τ->σ        gamam|-Z:τ                  T-app
+-------------------------   ----------------------------------------------
+GAMMA|-x: σ-->τv->σ                GAMMA|- x y z:σ
+--------------------------------------------------------------------------APP
+GAMMA=x:p,y:σ,z:τ|-x(xyz):τ->σ 
+--------------------------------------------------------------------------T_abs
+x:p,y:σ|-λz : τ.x(xyz):τ->τ->σ 
+--------------------------------------------------------------------------T_ABS
+x:p|-λy : σ.λz : τ.x(xyz). :σ->τ->τ->σ
+-------------------------------------------------------------------------- T-ABS
+|-λx : ρ.λy : σ.λz : τ.x(xyz) : (:σ->τ->σ)->σ->τ->τ->σ
+(identificando qué tipos pueden ser τ , σ y ρ)
 
 
 
+                            ------------------axv ---------------axv
+                            GAMMA|-G:sigma->tau  GAMMA|-x:sigma
+---------------axv          ---------------------------T-APP
+GAMMA|-F:tau:ro             GAMMA|-g x: tau 
+------------------------------------------------------------------Tapp
+GAMMA=f:tau->ro,g:sigma->tau,x:sigma |- f (g x):ro
+------------------------------------------------------------------T-abs
+f:tau->ro,g:sigma->tau|-λx:sigma.F(g x):sigma->ro
+-------------------------------------------------------------------T-ABS
+F:tau->ro|-λg:sigma->tau.λx:sigma. F(gx) :(sigma->tau)->sigma->ro
+-------------------------------------------- T-abs
+|-λf:tau->ro.λg:sigma->tau.λx:sigma. F(G X)
 
 
+((λx:bool.λy:bool. if x then true else y)false)true ->Beta 
+(y:bool. if false then true else y) true ->Beta
+if false then true else true  -> iff
+true (termino 0)
 
 
+(λx:bool.λy:bool->bool.y(yx))((λz:bool.true)false)(λw:bool.w)
+->APPi,APPr,B (λx:bool.λy:bool->bool.y(yx)) true (λw:b.w)
+->(λy:bool->bool.y(y true)) w
+-> w (w true) -> true 
 
 
+EJERCICIO probar que la semantica operacionas de calculo lambda con booleanos 
+con la estrategia ce call by value esta determinada 
+es decir probar que si M->M1 y M->M2 entonces m1=m2
+
+casos bases  ift iff beta (reglas de computo)
+B:M=(λx:sigma.M') V 
+M1= M{x:=V}
+savemos que M tambien reduce a m2 
+peero solo podemos usar esta regla para reducir a M 
+por ende solo se puede aplicar a esta y queda lo mismo 
+QED 
+
+caso inductivo: reglas de congruencia 
+todas tienen la premisa M'->m HI di M'->n1
+y m´->n2 entonce n1=n2 s
+
+EXTENSION CON NUMEROS NATURALES
+σ ::= . . . | Nat
+M ::= . . . | zero | succ(M ) | pred(M ) | isZero(M )
+
+-------------ax0
+Γ |- zero : Nat 
+
+Γ |- M : Nat
+------------------succ
+Γ |- succ(M ) : Nat 
+
+Γ |- M:nat
+-------------------pred
+Γ |- pred(M ) : Nat
+
+Γ |- M : Nat
+-----------------------isZero
+Γ |- isZero(M ) : Boo
+
+V ::= . . . | zero | succ(V )
+pred(succ(V )) → V          (pred)
+isZero(zero) → true         (isZero0)
+isZero(succ(V )) → false    (isZeron)
+
+Si M → N , entonces:
+succ(M ) → succ(N )         (succc)
+pred(M ) → pred(N )         (predc)
+isZero(M ) → isZero(N )     (isZeroc)
 
 
+----------------------------------
+cambiando regals semanticas
+supongamos que agregamos las siguiente regla para las abstracciones 
+si m->n entonces 
+λx:t.M->λx:t.n 
 
+| repensal cl cjto de valores para respetar esta modificacion pensar por ejemplo si 
+λx:bool id_bool true es o no un valor  ¿y λx:bool?
+x?
+
+V::=true|False|λx:sigma.F|0|succ(V) donde F es una forma normal
+
+que reglas deberian modificarse para no romper el determinismo?
+(λx : σ.F )V → F {x := V } (β)
 
 
 -}
